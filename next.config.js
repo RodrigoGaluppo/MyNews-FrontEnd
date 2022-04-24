972f78524aeb62e88b497eec58f23adfb73aa65a
rodrigo <rodrigogaluppo788@gmail.com>
2022-04-24 23:59:46 +0100

    enable pwa

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 const withPWA = require("next-pwa")
 
 module.exports = withPWA({
+  target:"serverless",
   pwa:{
     dest:"public",
-    target:"serverless",
     disable: process.env.NODE_ENV === "development",
     fallbacks: {
       document: '/src/pages/404.tsx',
