49a0188a0275f960a5492faa8aa425a47f4c163d
rodrigo <rodrigogaluppo788@gmail.com>
2022-04-25 00:20:13 +0100

    enable pwa fix bugs next config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
 const withPWA = require("next-pwa")
 
 module.exports = withPWA({
-  target:"serverless",
   pwa:{
     dest:"public",
     disable: process.env.NODE_ENV === "development",
-    fallbacks: {
-      document: "/pages/404.tsx"
-    }
   },
 
   reactStrictMode: true
